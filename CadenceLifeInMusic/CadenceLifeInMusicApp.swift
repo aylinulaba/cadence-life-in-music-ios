4e8f6983b2f1de45f77cc14b95ff218fbbb49cac
Aylin <81568176+aylinulaba@users.noreply.github.com>
2025-09-01 21:28:43 +0300

    Initial Commit

---
+++ CadenceLifeInMusic/CadenceLifeInMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadenceLifeInMusicApp.swift
+//  CadenceLifeInMusic
+//
+//  Created by Aylin ULABA on 1.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CadenceLifeInMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CadenceLifeInMusic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CadenceLifeInMusic
+//
+//  Created by Aylin ULABA on 1.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
